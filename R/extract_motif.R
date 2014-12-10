57493feefc5b0abe6fdb2b299c3631c7ed791692
philliplab <philliplab@gmail.com>
2014-12-10 15:45:07 +0200

    first draft of extract_motif

---
+++ R/extract_motif.R
@@ -0,0 +1,37 @@
+library(Biostrings)
+library(ShortRead)
+
+x <- readFastq("/home/phillipl/projects/MotifBinner/data/CAP177_2040_v1merged.fastq")
+x <- x@sread
+seq_data <- x
+prefix <- "CCAGCTGGTTATGCGATTCTMARGTG"
+suffix <- "CTGAGCGTGTGGCAAGGCCC"
+motif_length <- 9
+max.mismatch <- 5
+fixed = FALSE
+
+extract_motif <- function(seq_data, prefix, suffix, motif_length, max.mismatch = 5,
+                          fixed = FALSE){
+  seq_data <- DNAStringSet(gsub("[^ACGT]", "+", seq_data))
+  motif_n <- paste(rep("N", motif_length), collapse="")
+  padded_motif <- DNAString(paste0(prefix, motif_n, suffix))
+  matches <- vmatchPattern(padded_motif, 
+                           seq_data, 
+                           max.mismatch = max.mismatch, 
+                           with.indels=FALSE, 
+                           fixed = fixed)
+  matching_seq <- sapply(matches, length)
+  stopifnot(all(matching_seq < 2))
+  matching_seq <- matching_seq != 0
+  matched_seq <- seq_data[matching_seq]
+  unmatched_seq <- seq_data[!matching_seq]
+  matches <- unlist(matches)
+#  motifs <- subseq(matched_seq, 
+#                   start(matches) + nchar(prefix), 
+#                   stop(matches) - nchar(suffix))
+  shifted_matches <- matches
+ start(shifted_matches) <- start(shifted_matches) + nchar(prefix)
+ end(shifted_matches) <- end(shifted_matches) - nchar(suffix)
+ padAndClip(matched_seq, shifted_matches, Lpadding.letter="+", Rpadding.letter="+")
+#  return(motifs)
+}
