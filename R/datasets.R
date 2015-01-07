6fc68a64b92b2f119f0bb3b887ea16b3b66263dc
philliplab <philliplab@gmail.com>
2015-01-07 14:14:42 +0200

    add first test data and basic testing template

---
+++ R/datasets.R
@@ -0,0 +1,11 @@
+#' Returns that dataset used for testing the mislabel detection
+#' @export
+
+get_mislabel_test_data <- function(){
+  x <- list('test1' = list('in' = DNAStringSet(c('AAAAAAAAAAAAAAAAAAAAAAAAAAAAA',
+                                               'AAAAAAAAAAAAAAAAAAAAAAAAAAAAA')),
+                           'out' = DNAStringSet('CCCCCCCCCCCCCCCCCCCCCCCCCCCCC')
+                           )
+  )
+  return(x)
+}
