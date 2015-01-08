d7f993b7de0f8586b744bc91d80da1fafe07577d
philliplab <philliplab@gmail.com>
2015-01-08 10:51:52 +0200

    update random classification strategy; build package

--- R/classify_mislabelling.R
+++ R/classify_mislabelling.R
@@ -5,7 +5,7 @@
 #' interest and the other representing reads that were from a different
 #' molecule.
 #'
-#' @value
+#' @return
 #' A list with two elements 'in' and 'out'
 #' @param bin The input bin as a single DNAStringSet.
 #' @param technique A string selecting which technique to use for the
@@ -17,12 +17,26 @@
 classify_bin <- function(bin, technique = 'random', params = list(n=0.2)){
 }
 
-#' Randomly classify a number of sequences to the 'out' group
+#' Randomly classify a number of unique sequences to the 'out' group
 #'
-#' A very poor strategy included as a baseline
-#' @inheritParams classify_bin
+#' A very poor strategy included as a baseline. 
+#'
+#' First all the unique sequences in a bin is found. Then a percentage of then
+#' is assigned to the unique-out group. Then all sequences in the original bin
+#' that match sequences in the unique-out group is moved to the out bin.
+#'
+#' @param bin The input bin as a single DNAStringSet.
+#' @param n The proportion of the unique sequences in the bin to remove.
 #' @export
 
-classify_bin_random <- function(bin, params){
-  return(1)
+classify_bin_random <- function(bin, n){
+  ubin <- unique(bin)
+  n_elements <- length(ubin)
+  n_to_remove <- trunc(n * n_elements)
+  to_remove <- sample(1:n_elements, n_to_remove)
+  uin <- ubin[-to_remove]
+  uout <- ubin[to_remove]
+
+  return(list('in' = bin[bin %in% uin],
+              'out' = bin[bin %in% uout]))
 }
