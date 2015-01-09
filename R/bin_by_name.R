284fd31573df8a1a2fd81f2a3cd32952244bada2
philliplab <philliplab@gmail.com>
2015-01-09 10:42:05 +0200

    expand bin_by_name to indicate sequence uniqness

--- R/bin_by_name.R
+++ R/bin_by_name.R
@@ -1,10 +1,22 @@
 #' Groups sequences together into bins based on their names
+#' @param seq_dat The sequence data whose names indicate what bins they belong
+#' to
+#' @param add_sequence_uniqueness_id If True, an integer will be appended to
+#' the end of each sequence's name so that all identical sequences in a bin
+#' gets the same number and sequences who are not identical will get different
+#' numbers.
 #' @export
 
-bin_by_name <- function(seq_dat){
+bin_by_name <- function(seq_dat, add_sequence_uniqueness_id = FALSE){
   bins <- list()
   for (motif in unique(names(seq_dat))){
-    bins[[motif]] <- seq_dat[names(seq_dat) == motif]
+    cbin <- seq_dat[names(seq_dat) == motif]
+    if (add_sequence_uniqueness_id){
+      names(cbin) <- paste(names(cbin), match(cbin, unique(cbin)),
+                           sep = "_")
+      cbin <- cbin[order(names(cbin))]
+    }
+    bins[[motif]] <- cbin
   }
   return(bins)
 }
