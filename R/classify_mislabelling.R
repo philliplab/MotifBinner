b73ea1c7be5aef4b58cd342af8d3e296992ee278
philliplab <philliplab@gmail.com>
2015-01-13 11:32:46 +0200

    add a rough version of the information variance balance classification strategy

--- R/classify_mislabelling.R
+++ R/classify_mislabelling.R
@@ -20,6 +20,9 @@ classify_bin <- function(bin, technique = 'random', params = list(n=0.2)){
   if (technique == 'random'){
     classified <- do.call(classify_bin_random, params)
   }
+  if (technique == 'infovar_balance'){
+    classified <- do.call(classify_bin_infovar_balance, params)
+  }
   return(classified)
 }
 
@@ -51,3 +54,47 @@ classify_bin_random <- function(bin, n){
   return(list('src' = bin[bin %in% usrc],
               'out' = bin[bin %in% uout]))
 }
+
+#' Hi there
+#' @export
+
+classify_bin_infovar_balance <- function(bin, params = NULL){
+  dists <- NULL
+  bin_dists <- stringDist(bin)
+  dmat <- as.matrix(bin_dists)
+  row.names(dmat) <- 1:nrow(dmat)
+  removed_sequences <- NULL
+  orig_dmat <- dmat
+  pdr_psr_rat <- 1000
+  threshold <- 2
+  while((nrow(unique(dmat)) > 1) & (pdr_psr_rat > threshold)){
+    dists <- c(dists, mean(dmat))
+    dvec <- apply(dmat, 1, sum)
+    max_indx <- which(dvec == max(dvec))
+    new_dmat <- dmat[-max_indx, -max_indx]
+    
+#    print(dim(dmat))
+#    print(mean(dmat))
+#    print(max_indx)
+#    print('percentage sequences removed')
+#    psr <- length(max_indx) / nrow(dmat)
+#    print(psr)
+#    print('percentage distance reduction')
+#    pdr <- (mean(dmat) - mean(new_dmat)) / mean(orig_dmat)
+#    print(pdr)
+#    pdr_psr_rat <- pdr / psr
+#    print(pdr_psr_rat)
+#    print('--------------------------------------')
+
+    if (pdr_psr_rat > threshold){
+      removed_sequences <- c(removed_sequences, row.names(dmat)[max_indx])
+      dmat <- new_dmat
+    }
+  }
+  src_seq <- bin[as.integer(row.names(dmat))]
+  out_seq <- bin[as.integer(removed_sequences)]
+  return(list(src = src_seq,
+              out = out_seq))
+}
+
+
