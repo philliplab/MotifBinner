761750e91278cd2a7dfde62678a30e800b0e50d5
philliplab <philliplab@gmail.com>
2015-02-17 12:24:06 +0200

    add basic tests for most frequent classifier

--- tests/testthat/test_most_freq_classifier.R
+++ tests/testthat/test_most_freq_classifier.R
@@ -3,18 +3,13 @@ context('Most Frequent Classifier')
 test_that('The most frequent classifier works', {
   bin <- DNAStringSet(c(rep('AA', 5), rep('CC', 10), 
                         rep('GG', 2), rep('TT', 1)))
-  classified <- classify_bin_random(bin, 0.25)
+  classified <- classify_bin_most_frequent(bin)
   expect_that(check_classification(bin, classified), is_true())
-  expect_that(length(classified$src) > 0, is_true())
-  expect_that(length(classified$out) > 0, is_true())
+  expect_that(length(classified$src) == 10, is_true())
+  expect_that(length(classified$out) == 8, is_true())
 
-  classified <- classify_bin(bin, technique='random', params=list(n= 0.25))
+  classified <- classify_bin(bin, technique='most_frequent', params=list())
   expect_that(check_classification(bin, classified), is_true())
-  expect_that(length(classified$src) > 0, is_true())
-  expect_that(length(classified$out) > 0, is_true())
-
-  classified <- classify_bin_random(bin, 0)
-  expect_that(check_classification(bin, classified), is_true())
-  expect_that(length(classified$out) == 0, is_true())
+  expect_that(length(classified$src) == 10, is_true())
+  expect_that(length(classified$out) == 8, is_true())
 })
-
