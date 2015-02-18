e9fc8bea42d83a3eaaa68f591a13da0cb5dbe4f9
philliplab <philliplab@gmail.com>
2015-02-18 09:12:51 +0200

    add tests  for the motif extractor

--- tests/testthat/test_extract_motifs.R
+++ tests/testthat/test_extract_motifs.R
@@ -11,5 +11,23 @@ test_that('The motif extractor is working', {
                               suffix = 'CCAACCGCTC', max.mismatch=5)
   expect_that(names(extracted$matched_seq)[1]=='ACGTACGT', is_true())
   expect_that(names(extracted$matched_seq)[2]=='ACGTACGT', is_true())
+
+  extracted <- extract_motifs(seq_data, prefix = 'AACGAATTAA', motif_length=8, 
+                              suffix = 'CCAACCGCTC', max.mismatch=0)
+  expect_that(names(extracted$matched_seq)[1]=='ACGTACGT', is_true())
+  expect_that(names(extracted$matched_seq)[2]=='ACGTACGT', is_true())
+
+  seq_data <- DNAStringSet(c(
+          paste(c(paste0(sample(c('A', 'C', 'G', 'T'), 20, replace=T), collapse=""),
+                  'CACGAATTAA', 'ACGTACGT', 'CCAACCGCTC'), collapse = ""),
+          paste(c(paste0(sample(c('A', 'C', 'G', 'T'), 20, replace=T), collapse=""),
+                  'CACGAATTAA', 'ACGTACGT', 'CCAACCGCTC'), collapse = "")
+                        ))
+  extracted <- extract_motifs(seq_data, prefix = 'AACGAATTAA', motif_length=8, 
+                              suffix = 'CCAACCGCTC', max.mismatch=0)
+
+  expect_that(length(extracted$matched_seq) == 0, is_true())
+  expect_that(length(extracted$unmatched_seq) == 2, is_true())
+
 })
 
