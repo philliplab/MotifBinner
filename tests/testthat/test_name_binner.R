af7624671c11a595b890372723cdc076c733b8c3
philliplab <philliplab@gmail.com>
2015-02-18 12:00:00 +0200

    expand tests for bin_by_name

--- tests/testthat/test_name_binner.R
+++ tests/testthat/test_name_binner.R
@@ -1,6 +1,6 @@
 context('Name Binner')
 
-test_that('The name binner works', {
+test_that('The name binner works with 2 bins', {
   motif_extracted <- DNAStringSet(c('AAA', 'AAA', 'AAC', 'CCC'))
   names(motif_extracted) <- c('a', 'a', 'a', 'b')
   binned <- bin_by_name(motif_extracted)
@@ -9,5 +9,23 @@ test_that('The name binner works', {
   expect_that(length(binned$b), equals(1))
   expect_that(unname(as.character(binned$b)), equals('CCC'))
 
+})
+
+test_that('The name binner works with 1 bins', {
+  motif_extracted <- DNAStringSet(c('AAA', 'AAA', 'AAC', 'CCC'))
+  names(motif_extracted) <- c('a', 'a', 'a', 'a')
+  binned <- bin_by_name(motif_extracted)
+  expect_that(all(sort(names(binned)) == c('a')), is_true())
+  expect_that(length(binned$a), equals(4))
+})
+
+test_that('The name binner works with size 1 bins', {
+  motif_extracted <- DNAStringSet(c('AAA', 'TAA', 'AAC', 'CCC'))
+  names(motif_extracted) <- c('a', 'b', 'c', 'd')
+  binned <- bin_by_name(motif_extracted)
+  expect_that(all(sort(names(binned)) == c('a', 'b', 'c', 'd')), is_true())
+  expect_that(length(binned$a), equals(1))
+  expect_that(length(binned$b), equals(1))
+  expect_that(unname(as.character(binned$b)), equals('TAA'))
 })
 
