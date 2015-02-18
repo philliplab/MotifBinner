08d9fb279737f482901bd39f7625fcef41da71ae
philliplab <philliplab@gmail.com>
2015-02-18 10:58:12 +0200

    refactor motif extractor tests and add sequence renaming test

--- tests/testthat/test_extract_motifs.R
+++ tests/testthat/test_extract_motifs.R
@@ -1,6 +1,6 @@
 context('Extract Motifs')
 
-test_that('The motif extractor is working', {
+test_that('The motif extractor works when motifs identifiers match perfectly', {
   seq_data <- DNAStringSet(c(
           paste(c(paste0(sample(c('A', 'C', 'G', 'T'), 20, replace=T), collapse=""),
                   'AACGAATTAA', 'ACGTACGT', 'CCAACCGCTC'), collapse = ""),
@@ -11,11 +11,15 @@ test_that('The motif extractor is working', {
                               suffix = 'CCAACCGCTC', max.mismatch=5)
   expect_that(names(extracted$matched_seq)[1]=='ACGTACGT', is_true())
   expect_that(names(extracted$matched_seq)[2]=='ACGTACGT', is_true())
+  expect_that(width(extracted$matched_seq)[1] == 20, is_true())
 
   extracted <- extract_motifs(seq_data, prefix = 'AACGAATTAA', motif_length=8, 
                               suffix = 'CCAACCGCTC', max.mismatch=0)
   expect_that(names(extracted$matched_seq)[1]=='ACGTACGT', is_true())
   expect_that(names(extracted$matched_seq)[2]=='ACGTACGT', is_true())
+})
+
+test_that('The motif extractor works when motif identifiers have errors', {
 
   seq_data <- DNAStringSet(c(
           paste(c(paste0(sample(c('A', 'C', 'G', 'T'), 20, replace=T), collapse=""),
