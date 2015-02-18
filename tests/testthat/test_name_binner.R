298b601fd37d2a91566a6dafa836291056dc8622
philliplab <philliplab@gmail.com>
2015-02-18 11:06:40 +0200

    add basic tests for the bin_by_name function

---
+++ tests/testthat/test_name_binner.R
@@ -0,0 +1,13 @@
+context('Name Binner')
+
+test_that('The name binner works', {
+  motif_extracted <- DNAStringSet(c('AAA', 'AAA', 'AAC', 'CCC'))
+  names(motif_extracted) <- c('a', 'a', 'a', 'b')
+  binned <- bin_by_name(motif_extracted)
+  expect_that(all(sort(names(binned)) == c('a', 'b')), is_true())
+  expect_that(length(binned$a), equals(3))
+  expect_that(length(binned$b), equals(1))
+  expect_that(unname(as.character(binned$b)), equals('CCC'))
+
+})
+
