7761c5d5acb3f755515bbc12eab167c42a8344d3
philliplab <philliplab@gmail.com>
2015-02-19 08:30:45 +0200

    add test skeletons for the aligner, consensus constructor and bin processor

---
+++ tests/testthat/test_process_bin.R
@@ -0,0 +1,20 @@
+context('Process Bin')
+
+test_that('The bin processor works', {
+  bin <- DNAStringSet(c(rep('AA', 5), rep('CC', 10), 
+                        rep('GG', 2), rep('TT', 1)))
+  classified <- classify_bin_random(bin, 0.25)
+  expect_that(check_classification(bin, classified), is_true())
+  expect_that(length(classified$src) > 0, is_true())
+  expect_that(length(classified$out) > 0, is_true())
+
+  classified <- classify_bin(bin, technique='random', params=list(n= 0.25))
+  expect_that(check_classification(bin, classified), is_true())
+  expect_that(length(classified$src) > 0, is_true())
+  expect_that(length(classified$out) > 0, is_true())
+
+  classified <- classify_bin_random(bin, 0)
+  expect_that(check_classification(bin, classified), is_true())
+  expect_that(length(classified$out) == 0, is_true())
+})
+
