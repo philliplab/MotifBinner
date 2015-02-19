de254d116e4c29f6894dd9a3905f01ca07e9ba88
philliplab <philliplab@gmail.com>
2015-02-19 10:06:49 +0200

    add basic tests for the aligner

--- tests/testthat/test_alignment.R
+++ tests/testthat/test_alignment.R
@@ -1,20 +1,15 @@
 context('Alignment')
 
 test_that('The aligner works', {
-  bin <- DNAStringSet(c(rep('AA', 5), rep('CC', 10), 
-                        rep('GG', 2), rep('TT', 1)))
-  classified <- classify_bin_random(bin, 0.25)
-  expect_that(check_classification(bin, classified), is_true())
-  expect_that(length(classified$src) > 0, is_true())
-  expect_that(length(classified$out) > 0, is_true())
+  seqs <- DNAStringSet(rep(paste(rep('A', 50), collapse=""), 5))
+  aligned <- align_sequences(seqs)
+  expect_that(all(seqs == unname(aligned)), is_true())
 
-  classified <- classify_bin(bin, technique='random', params=list(n= 0.25))
-  expect_that(check_classification(bin, classified), is_true())
-  expect_that(length(classified$src) > 0, is_true())
-  expect_that(length(classified$out) > 0, is_true())
-
-  classified <- classify_bin_random(bin, 0)
-  expect_that(check_classification(bin, classified), is_true())
-  expect_that(length(classified$out) == 0, is_true())
+  seqs <- DNAStringSet(c('AAAAAAAAAAATGAAACGTTAGCGCGTTTTTTTTTT',
+                         'AAAAAAAAAAATGCGTTAGCGCGTTTTTTTTTT'))
+  aligned <- align_sequences(seqs)
+  target <- DNAStringSet(c('AAAAAAAAAAATGAAACGTTAGCGCGTTTTTTTTTT',
+                           'AAAAAAAAAAATG---CGTTAGCGCGTTTTTTTTTT'))
+  expect_that(all(target == aligned), is_true())
 })
 
