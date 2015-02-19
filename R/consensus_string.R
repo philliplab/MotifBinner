7e058da2120e188938b9082f2dfceddd634c29af
philliplab <philliplab@gmail.com>
2015-02-19 10:26:09 +0200

    update consensus constructors documentation to explain naming behaviour

--- R/consensus_string.R
+++ R/consensus_string.R
@@ -1,4 +1,12 @@
 #' Constructs a consensus string using the specified technique
+#' 
+#' This function is specifically designed to work in the situation of binning
+#' sequences based on PIDS, so it handles sequence names in a specific way. It
+#' assumes that the name of the first sequence is the name that needs to be
+#' given to the consensus sequence. Furthermore, it will append and underscore
+#' and the number of sequences that were used to construct the consensus to the
+#' end of this name.
+#'
 #' @param seqs The aligned sequences to construct a consensus of
 #' @param technique The technique to use
 #' @param params Extra parameters passed to the consensus string construction
