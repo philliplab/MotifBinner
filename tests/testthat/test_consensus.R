8c2d99ebfa6776afad96485aa98a831a7166e62f
philliplab <philliplab@gmail.com>
2015-02-26 13:11:05 +0200

    add tests for the wonky behaviour of Biostrings:consensusString when there are gaps

--- tests/testthat/test_consensus.R
+++ tests/testthat/test_consensus.R
@@ -28,4 +28,29 @@ test_that('The consensus string constructor works', {
   consen <- construct_consensus(seqs)
   target <- DNAString('AAAAAAAAAAW')
   expect_that(consen[[1]] == target, is_true())
+
+  seqs <- DNAStringSet(c('AAAAAAAAAAA',
+                         'AAAAAAAAAAA',
+                         'AAAAAAAAAAA',
+                         'AAAAAAAAAAA',
+                         'AAAAAAAAAAT'))
+  consen <- construct_consensus(seqs)
+  target <- DNAString('AAAAAAAAAAA')
+  expect_that(consen[[1]] == target, is_true())
+
+  seqs <- DNAStringSet(c('AAAA-AAAAAA',
+                         'AAAAAAAAAAA',
+                         'AAAAAAAAAAA',
+                         'AAAAAAAAAAA',
+                         'AAAAAAAAAAT'))
+  consen <- construct_consensus(seqs, params=list(ambiguityMap='N'))
+  target <- DNAString('AAAAAAAAAAA')
+  expect_that(consen[[1]] == target, is_true())
+
+  seqs <- DNAStringSet(c('AAAA-AAAAAA',
+                         'AAAAAAAAAAA',
+                         'AAAAAAAAAAT'))
+  consen <- construct_consensus(seqs, params=list(ambiguityMap='N'))
+  target <- DNAString('AAAANAAAAAN')
+  expect_that(consen[[1]] == target, is_true())
 })
