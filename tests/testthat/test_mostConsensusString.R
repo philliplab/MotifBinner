d82dfca09e8c09d8d5d19ffed1d831068b0748fc
philliplab <philliplab@gmail.com>
2015-02-27 07:50:45 +0200

    refine mostConsensus gap tests

--- tests/testthat/test_mostConsensusString.R
+++ tests/testthat/test_mostConsensusString.R
@@ -50,27 +50,33 @@ test_that('most consensus string can be called via construct_consensus', {
 })
 
 test_that('most consensus string plays nice with gaps',{
-  seqs <- DNAStringSet(c('-AAAAAAAAAA','AAAAAAAAAAA','AAAAAAAAAAA',
-                         'AAAAAAAAAAT','AAAAAAAAAAT','AAAAAAAAAAT',
-                         'AAAAAAAAAAC'))
+  seqs <- DNAStringSet(c('AAA','AAA','AAA', 'AAA'))
   consen <- construct_consensus(seqs, technique = 'mostConsensusString', params = list())
-  target <- DNAString('AAAAAAAAAAW')
+  target <- DNAString('AAA')
   expect_that(consen[[1]] == target, is_true())
-  expect_that(names(consen)[1] == '_7', is_true())
+  expect_that(names(consen)[1] == '_4', is_true())
 
-  seqs <- DNAStringSet(c('-AAAAAAAAAA','-AAAAAAAAAA','AAAAAAAAAAA',
-                         '-AAAAAAAAAT','-AAAAAAAAAT','AAAAAAAAAAT',
-                         '-AAAAAAAAAC'))
+  seqs <- DNAStringSet(c('AAA','AAA','AAA', '-AA'))
   consen <- construct_consensus(seqs, technique = 'mostConsensusString', params = list())
-  target <- DNAString('AAAAAAAAAAW')
+  target <- DNAString('AAA')
   expect_that(consen[[1]] == target, is_true())
-  expect_that(names(consen)[1] == '_7', is_true())
+  expect_that(names(consen)[1] == '_4', is_true())
 
-  seqs <- DNAStringSet(c('AAAAAAAAAAA','AAAAAAAAAAA','AAAAAAAAAAA',
-                         '-AAAAAAAAAT','-AAAAAAAAAT','-AAAAAAAAAT'))
+  seqs <- DNAStringSet(c('AAA','AAA','-AA', '-AA'))
   consen <- construct_consensus(seqs, technique = 'mostConsensusString', params = list())
-  target <- DNAString('AAAAAAAAAAW')
+  target <- DNAString('AA+')
   expect_that(consen[[1]] == target, is_true())
-  expect_that(names(consen)[1] == '_7', is_true())
+  expect_that(names(consen)[1] == '_4', is_true())
 
+  seqs <- DNAStringSet(c('AAA','AA-','AA-', 'AA-'))
+  consen <- construct_consensus(seqs, technique = 'mostConsensusString', params = list())
+  target <- DNAString('AA-')
+  expect_that(consen[[1]] == target, is_true())
+  expect_that(names(consen)[1] == '_4', is_true())
+
+  seqs <- DNAStringSet(c('AA-','AA-','AA-', 'AA-'))
+  consen <- construct_consensus(seqs, technique = 'mostConsensusString', params = list())
+  target <- DNAString('AA-')
+  expect_that(consen[[1]] == target, is_true())
+  expect_that(names(consen)[1] == '_4', is_true())
 })
