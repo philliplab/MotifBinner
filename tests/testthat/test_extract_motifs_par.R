683abeba7cfdb90d3c0a59da696a68f61f8346ba
philliplab <philliplab@gmail.com>
2015-03-27 13:39:41 +0200

    add tests for parallel motif finding

---
+++ tests/testthat/test_extract_motifs_par.R
@@ -0,0 +1,60 @@
+context('Extract Motifs Parallel')
+
+test_that('The motif extractor works when motifs identifiers match perfectly', {
+  seq_data <- DNAStringSet(c(
+          paste(c(paste0(sample(c('A', 'C', 'G', 'T'), 20, replace=T), collapse=""),
+                  'AACGAATTAA', 'ACGTACGT', 'CCAACCGCTC'), collapse = ""),
+          paste(c(paste0(sample(c('A', 'C', 'G', 'T'), 20, replace=T), collapse=""),
+                  'AACGAATTAA', 'ACGTACGT', 'CCAACCGCTC'), collapse = "")
+                        ))
+  extracted <- extract_motifs_par(seq_data, prefix = 'AACGAATTAA', motif_length=8, 
+                              suffix = 'CCAACCGCTC', max.mismatch=5)
+  expect_that(names(extracted$matched_seq)[1]=='ACGTACGT', is_true())
+  expect_that(names(extracted$matched_seq)[2]=='ACGTACGT', is_true())
+  expect_that(width(extracted$matched_seq)[1] == 20, is_true())
+
+  extracted <- extract_motifs_par(seq_data, prefix = 'AACGAATTAA', motif_length=8, 
+                              suffix = 'CCAACCGCTC', max.mismatch=0)
+  expect_that(names(extracted$matched_seq)[1]=='ACGTACGT', is_true())
+  expect_that(names(extracted$matched_seq)[2]=='ACGTACGT', is_true())
+})
+
+test_that('The motif extractor works when motif identifiers have errors', {
+
+  seq_data <- DNAStringSet(c(
+          paste(c(paste0(sample(c('A', 'C', 'G', 'T'), 20, replace=T), collapse=""),
+                  'CACGAATTAA', 'ACGTACGT', 'CCAACCGCTC'), collapse = ""),
+          paste(c(paste0(sample(c('A', 'C', 'G', 'T'), 20, replace=T), collapse=""),
+                  'CACGAATTAA', 'ACGTACGT', 'CCAACCGCTC'), collapse = "")
+                        ))
+  extracted <- extract_motifs_par(seq_data, prefix = 'AACGAATTAA', motif_length=8, 
+                              suffix = 'CCAACCGCTC', max.mismatch=0)
+
+  expect_that(length(extracted$matched_seq) == 0, is_true())
+  expect_that(length(extracted$unmatched_seq) == 2, is_true())
+  expect_that(names(extracted$unmatched_seq)[1]=='seq_1', is_true())
+  expect_that(names(extracted$unmatched_seq)[2]=='seq_2', is_true())
+
+  extracted <- extract_motifs_par(seq_data, prefix = 'AACGAATTAA', motif_length=8, 
+                              suffix = 'CCAACCGCTC', max.mismatch=1)
+
+  expect_that(names(extracted$matched_seq)[1]=='ACGTACGT', is_true())
+  expect_that(names(extracted$matched_seq)[2]=='ACGTACGT', is_true())
+  expect_that(length(extracted$unmatched_seq) == 0, is_true())
+  
+  seq_data <- DNAStringSet(c(
+          paste(c(paste0(sample(c('A', 'C', 'G', 'T'), 20, replace=T), collapse=""),
+                  'CACGAATTAA', 'ACGTACGT', 'CCAACCGCTC'), collapse = ""),
+          paste(c(paste0(sample(c('A', 'C', 'G', 'T'), 20, replace=T), collapse=""),
+                  'CTCGAATTAA', 'ACGTACGT', 'CCAACCGCTC'), collapse = "")
+                        ))
+  names(seq_data) <- c('s1', 's2')
+  extracted <- extract_motifs_par(seq_data, prefix = 'AACGAATTAA', motif_length=8, 
+                              suffix = 'CCAACCGCTC', max.mismatch=1)
+
+  expect_that(names(extracted$matched_seq)[1]=='ACGTACGT', is_true())
+  expect_that(names(extracted$unmatched_seq)[1]=='s2', is_true())
+  expect_that(length(extracted$unmatched_seq) == 1, is_true())
+  expect_that(width(extracted$matched_seq) < width(extracted$unmatched_seq), is_true())
+})
+
