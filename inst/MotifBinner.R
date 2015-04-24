1065e87a0d138e6578bc75d9917d12d8dfb14860
philliplab <philliplab@gmail.com>
2015-04-24 12:34:55 +0200

    add cleanup to the script that calls motifbinner from cmd

--- inst/MotifBinner.R
+++ inst/MotifBinner.R
@@ -20,9 +20,12 @@ make_option("--suffix",
 make_option("--motif_length", 
             type = 'numeric',
             help = "The length of the motif that forms the pid"),
+make_option("--max_mismatch_start", 
+            dest = 'max.mismatch_start', type = 'integer', default = 0,
+            help = "Maximum number of mismatches are iteratively increased when search - start at this number"),
 make_option("--max_mismatch", 
             dest = 'max.mismatch', type = 'integer',
-            help = "The maximum number of mismatches to allow when searching for the pid"),
+            help = "The maximum number of mismatches to allow when searching for the pid - iteratively incease the number of mismatches allowed until this number is reached."),
 make_option("--threshold", 
             type = 'numeric',
             help = "Outlier sequences are removed from the bin until the maximum distance between any two sequences drops below this threshold."),
@@ -30,8 +33,8 @@ make_option("--start_threshold",
             type = 'numeric',
             help = "Only start the classification if the maximum distance between and two sequences in the bin is greater than this."),
 make_option("--max_sequences", 
-            type = 'numeric',
-            help = "The maximum number of sequences to use for the computation of the distance matrix. If more sequences than this is present, then randomly select this many sequences and run the classification algorithm on them. This is only to improve the computation speed."),
+            type = 'numeric', default = 400,
+            help = "Warning: values larger than 500 might incur the wrath of the sysadmin. 400 is a good middle ground. The maximum number of sequences to use for the computation of the distance matrix. If more sequences than this is present, then randomly select this many sequences and run the classification algorithm on them. This is only to improve the computation speed."),
 make_option("--dont_remove_gaps", 
             dest = 'remove_gaps', action = 'store_false', default = TRUE,
             help = "use this option to prevent gaps from being removed from the consensus sequences"),
@@ -40,7 +43,10 @@ make_option("--dont_strip_uids",
             help = "Use this option to prevent the removal of the unique identifiers from the final consensus sequences. It is not intelligent. The names will be split on '_' and the first and last pieces will be kept."),
 make_option("--n_bins_to_process", 
             default = 0, type = 'integer',
-            help = "The number of bins to process through the outlier detection, alignment and consensus generation. If smaller than or equal to 0, all bins will be processed.")
+            help = "The number of bins to process through the outlier detection, alignment and consensus generation. If smaller than or equal to 0, all bins will be processed."),
+make_option("--verbose", 
+            default = FALSE, action = 'store_true',
+            help = "Adds fancy progress reporting via printouts to the screen and creation of files in the R'sessions tempory folder.")
                       )
 
 opt <- parse_args(OptionParser(option_list=option_list))
@@ -48,7 +54,8 @@ opt <- parse_args(OptionParser(option_list=option_list))
 opt$help <- NULL
 print(opt)
 dput(opt)
-do.call(process_file, opt)
+x <- do.call(process_file, opt)
+rm(x)
 
 # ./MotifBinner.R --file_name=~/projects/MotifBinner/data/CAP177_2040_v1merged.fastq --output=/tmp/MotifBinner --prefix=CCAGCTGGTTATGCGATTCTMARGTG --suffix=CTGAGCGTGTGGCAAGGCCC --motif_length=9 --max_mismatch=5 --threshold=0.01333 --start_threshold=0.01333 --max_sequences=100
 
